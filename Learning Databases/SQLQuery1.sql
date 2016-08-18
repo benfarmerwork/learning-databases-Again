59657e3a39cef470feb30aed6c9388343f95747e
Ben Farmer <ben_farmer3@hotmail.com>
2016-08-18 22:20:04 +0100

    Add files via upload

---
+++ Learning Databases/SQLQuery1.sql
@@ -0,0 +1,11 @@
+﻿--Insert into dbo.Students 
+--(Name,SecondName,Age,Gender)
+--Values ('Margi','Erringotn',65,'F')
+
+Select * From dbo.Students
+Where Age Between 18 And 63
+Order By Age Desc
+
+
+--Delete From dbo.Students 
+--Where id > 6
